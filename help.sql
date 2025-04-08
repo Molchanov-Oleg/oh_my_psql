451935dc984837f6060ad87f4813530cf642dcbc
Oleg Molchanov <o.molchanov@qiwi.com>
2025-04-09 00:57:55 +0300

    sort help topics

--- help.sql
+++ help.sql
@@ -4,4 +4,4 @@
 -- # author: Molchanov Oleg
 -- #
 
-\! cd "$OM_INSTALL_DIR";echo -n 'Enter help topic (* wildcard): ';read help_topic;head -vn1 $help_topic*.sql 2>&-|grep -v ^$|paste - -|grep '###'|sed 's/==> /:/;s/<==/\t/;s/\\echo//;s/\.sql//'
+\! cd "$OM_INSTALL_DIR";echo -n 'Enter help topic (* wildcard): ';read help_topic;head -vn1 $help_topic*.sql 2>&-|grep -v ^$|paste - -|grep '###'|sed 's/==> /:/;s/<==/\t/;s/\\echo//;s/\.sql//'|sort
