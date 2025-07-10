d4c765066d9d18bded40d956a9d11f5e45cd21ad
Oleg Molchanov <o.molchanov@qiwi.com>
2025-07-10 19:04:34 +0300

    add active process listing

---
+++ ps_a.sql
@@ -0,0 +1,22 @@
+\echo ### active db processes list in state not IDLE
+\echo
+-- #
+-- # author: Molchanov Oleg
+-- #
+
+select datname,
+       pid,
+       usename,
+       application_name,
+       client_addr,
+       client_hostname,
+       client_port,
+       backend_type,
+       to_char(backend_start, 'YYYY-MM-DD"T"HH24:MI:SS') backend_start,
+       backend_xmin,
+       wait_event,
+       state,
+       pg_blocking_pids(pid) as blocked_by
+  FROM pg_stat_activity
+  where state != 'idle'
+\g
